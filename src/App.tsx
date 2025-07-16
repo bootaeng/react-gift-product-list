7557ec3cb7546a0e8bcaf99eb1649b7be4fbfa30
bootaeng <tgwon0947@gmail.com>
2025-07-17 08:59:36 +0900

    Revert "1단계 코드리뷰 요청"
    This reverts commit 5b1355beb7dbe8440f1a03bc8f82f20d9d51c0a8, reversing
    changes made to 02ae762a740ebc3f48a2f0e3e16d2ba98da5644b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Layout } from '@/components/Layout'
 import styled from '@emotion/styled'
 import GiftFriendSelector from '@/components/GiftFriendSelector'
@@ -28,40 +27,6 @@ function App() {
       </Layout>
     </AppWrapper>
   );
-=======
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
->>>>>>> 151bc1bbd724e0a783aa25f114eb68ec0b0511d8
 }
 
 export default App
