7f2b2f773ef935ceef788348a4d20613c20857aa
bootaeng <tgwon0947@gmail.com>
2025-07-17 09:09:02 +0900

    fix:1단계 상태로 복구

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> parent of d882257 (fix:충돌 마커 제거)
 import path from 'path'
 
 export default defineConfig({
@@ -19,3 +23,24 @@ export default defineConfig({
     },
   },
 })
+<<<<<<< HEAD
+=======
+
+
+
+
+
+
+
+
+
+
+
+
+=======
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+>>>>>>> 151bc1bbd724e0a783aa25f114eb68ec0b0511d8
+>>>>>>> parent of d882257 (fix:충돌 마커 제거)
